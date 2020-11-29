f8302652b62049f0440e9392100a42bc0abaae96
Cazzzz <yeracscott9@gmail.com>
2020-11-29 19:00:39 +0000

    Scott Commit - Fragments

---
+++ app/src/main/java/com/example/self_madeapp/LoginFragment.kt
@@ -0,0 +1,9 @@
+package com.example.self_madeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import android.os.Bundle
+
+class LoginFragment : Fragment(R.layout.activity_login_fragment) {
+
+}
---
+++ app/src/main/java/com/example/self_madeapp/ProfileFragment.kt
@@ -0,0 +1,9 @@
+package com.example.self_madeapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class ProfileFragment : Fragment(R.layout.activity_profile_fragment){
+
+}
